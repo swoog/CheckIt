7ea075288e3b008aad4a51363cec62f1fb757545
Aurelien GALTIER <aurelien.galtier@live.fr>
2015-01-24 00:32:45 +0100

    Add check of name

---
+++ CheckIt.Tests/CheckNameTests.cs
@@ -0,0 +1,37 @@
+﻿namespace CheckIt.Tests
+{
+    using Xunit;
+
+    public class CheckNameTests
+    {
+        [Fact]
+        public void Should_check_name_of_class()
+        {
+            Check.Assembly("CheckIt.Tests.Data").Class("Class1").Name().Matche("^[A-Z].+$");
+        }
+
+        [Fact]
+        public void Should_throw_error_when_name_no_matche()
+        {
+            var e = Assert.Throws<MatchException>(
+                () =>
+                {
+                    Check.Assembly("CheckIt.Tests.Data").Class("Class1").Name().Matche("^[a-z].+$");
+                });
+
+            Assert.Equal("The folowing class doesn't respect pattern '^[a-z].+$' :\nClass1", e.Message);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_no_assembly_matche()
+        {
+            var e = Assert.Throws<MatchException>(
+                () =>
+                {
+                    Check.Assembly("Toto");
+                });
+
+            Assert.Equal("No assembly found that match 'Toto'", e.Message);
+        }
+    }
+}
