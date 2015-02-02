51bab108e10804a4a9c9ed36cd4d7f78571b1625
Aurelien GALTIER <aurelien.galtier@live.fr>
2015-02-02 21:09:18 +0100

    Add file

---
+++ CheckIt.StyleCop/StyleCopExtention.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckIt.StyleCop
+{
+    public static class StyleCopExtention
+    {
+        public static CheckStyleCop StyleCop(this CheckSources sources)
+        {
+            return new CheckStyleCop();
+        }
+    }
+
+    public class CheckStyleCop
+    {
+        public void SA1300()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
