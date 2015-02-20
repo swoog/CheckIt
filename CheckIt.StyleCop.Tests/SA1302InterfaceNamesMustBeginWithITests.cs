d417ba43c17f89bdd28a25b908ed90dcb8a78380
Aurelien GALTIER <aurelien.galtier@live.fr>
2015-02-20 23:47:41 +0100

    Add stylecop rules

---
+++ CheckIt.StyleCop.Tests/SA1302InterfaceNamesMustBeginWithITests.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckIt.StyleCop.Tests
+{
+    using Xunit;
+
+    public class SA1302InterfaceNamesMustBeginWithITests
+    {
+        public SA1302InterfaceNamesMustBeginWithITests()
+        {
+            Check.SetBasePathSearch(@"..\..\..\");
+        }
+
+        [Fact]
+        public void Should_interface_begin_upper_case_when_rule_is_selected()
+        {
+            Check.Extend().StyleCop().SA1302();
+        }
+    }
+}
