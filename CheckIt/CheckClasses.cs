e66eea02b800346600bed8f3a36e7eb7341bd062
Aurélien GALTIER <aurelien.galtier@live.fr>
2015-09-09 14:14:40 +0200

    Add ClassesObjectsFinder

--- CheckIt/CheckClasses.cs
+++ CheckIt/CheckClasses.cs
@@ -8,29 +8,36 @@ namespace CheckIt
 
     internal class CheckClasses : CheckTypes<IClass, IClassMatcher, ICheckClasses, ICheckClassesContains>, ICheckClasses, IClassMatcher, IObjectsFinder
     {
+        private readonly ClassesObjectsFinder classesObjectsFinder;
+
         public CheckClasses(IEnumerable<IClass> classes)
             : base(classes, "class")
         {
+            this.classesObjectsFinder = new ClassesObjectsFinder(this);
         }
 
         public CheckClasses(ICompilationDocument document, ICompilationInfo compile, string pattern)
             : base(document, compile, pattern, "class")
         {
+            this.classesObjectsFinder = new ClassesObjectsFinder(this);
         }
 
         public CheckClasses(string pattern)
             : base(pattern, "class")
         {
+            this.classesObjectsFinder = new ClassesObjectsFinder(this);
         }
 
         public CheckClasses(ICompilationInfo compilationInfo, string pattern)
             : base(compilationInfo, pattern, "class")
         {
+            this.classesObjectsFinder = new ClassesObjectsFinder(this);
         }
 
         private CheckClasses(IObjectsFinder objectsFinder)
             : this(objectsFinder.ToList<IClass>())
         {
+            this.classesObjectsFinder = new ClassesObjectsFinder(this);
         }
 
         public ICheckContains<ICheckClassesContains> Contains()
@@ -65,37 +72,37 @@ namespace CheckIt
 
         public IObjectsFinder Class(string pattern)
         {
-            throw new System.NotImplementedException();
+            return this.classesObjectsFinder.Class(pattern);
         }
 
         public IObjectsFinder Reference(string pattern)
         {
-            throw new System.NotImplementedException();
+            return this.classesObjectsFinder.Reference(pattern);
         }
 
         public IObjectsFinder Assembly(string pattern)
         {
-            throw new System.NotImplementedException();
+            return this.classesObjectsFinder.Assembly(pattern);
         }
 
         public IObjectsFinder File(string pattern)
         {
-            throw new System.NotImplementedException();
+            return this.classesObjectsFinder.File(pattern);
         }
 
         public IObjectsFinder Interfaces(string pattern)
         {
-            throw new System.NotImplementedException();
+            return this.classesObjectsFinder.Interfaces(pattern);
         }
 
         public IObjectsFinder Method(string pattern)
         {
-            return new CheckMethods(this.SelectMany(c => c.Method(pattern)), pattern);
+            return this.classesObjectsFinder.Method(pattern);
         }
 
         public List<T> ToList<T>()
         {
-            return this.Cast<T>().ToList();
+            return this.classesObjectsFinder.ToList<T>();
         }
     }
 }
---
+++ CheckIt/ClassesObjectsFinder.cs
@@ -0,0 +1,50 @@
+namespace CheckIt
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class ClassesObjectsFinder
+    {
+        private readonly CheckClasses checkClasses;
+
+        public ClassesObjectsFinder(CheckClasses checkClasses)
+        {
+            this.checkClasses = checkClasses;
+        }
+
+        public IObjectsFinder Class(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IObjectsFinder Reference(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IObjectsFinder Assembly(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IObjectsFinder File(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IObjectsFinder Interfaces(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IObjectsFinder Method(string pattern)
+        {
+            return new CheckMethods(this.checkClasses.SelectMany(c => c.Method(pattern)), pattern);
+        }
+
+        public List<T> ToList<T>()
+        {
+            return this.checkClasses.Cast<T>().ToList();
+        }
+    }
+}
